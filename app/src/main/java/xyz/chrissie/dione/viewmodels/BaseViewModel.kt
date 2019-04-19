a286226ed380bb9ab01e16982708068f0604d742
christinah <christinewasabi@gmail.com>
2019-04-19 14:57:41 +0300

    base view model for mavericks

---
+++ app/src/main/java/xyz/chrissie/dione/viewmodels/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package xyz.chrissie.dione.viewmodels
+
+import com.airbnb.mvrx.BaseMvRxViewModel
+import com.airbnb.mvrx.BuildConfig
+import com.airbnb.mvrx.MvRxState
+
+abstract class BaseViewModel<S : MvRxState>(initialState: S) :
+    BaseMvRxViewModel<S>(initialState, debugMode = BuildConfig.DEBUG) {
+
+}
